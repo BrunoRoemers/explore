6cc241e9ef593cc8eac008f8d220b6c4741e7c68
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2024-05-15 12:48:05 +0200

    Set svelte base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { PUBLIC_URL_PREFIX } from '$env/static/public';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+			base: PUBLIC_URL_PREFIX ?? ''
+		}
 	},
 	preprocess: vitePreprocess()
 };
