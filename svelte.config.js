3b65157e402a0d57b6748ccd8c90e6d70a177006
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2024-05-15 12:49:37 +0200

    hardcode base for now

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import { PUBLIC_URL_PREFIX } from '$env/static/public';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: PUBLIC_URL_PREFIX ?? ''
+			// TODO make dynamic
+			base: '/explore'
 		}
 	},
 	preprocess: vitePreprocess()
