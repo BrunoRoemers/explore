82b2b7aad90c84e44a75bf4654cef545b3a8ce27
BrunoRoemers <BrunoRoemers@users.noreply.github.com>
2024-05-16 21:15:09 +0000

    deploy: d045e384a8a611d80d4040c0cf0343af883198cd

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,37 @@
-const c = [
+const r = [
   "/explore/_app/immutable/assets/_layout-faf2005a.css",
-  "/explore/_app/immutable/chunks/6-ee6d2496.js",
-  "/explore/_app/immutable/chunks/singletons-c9b53750.js",
-  "/explore/_app/immutable/components/pages/_page.svelte-5bbbfaa2.js",
-  "/explore/_app/immutable/chunks/CollectionList-8eff242c.js",
-  "/explore/_app/immutable/chunks/CalendarList-5a7545ee.js",
-  "/explore/_app/immutable/chunks/0-05ca907b.js",
-  "/explore/_app/immutable/chunks/1-507f4667.js",
-  "/explore/_app/immutable/modules/pages/_layout.js-e0c98329.js",
+  "/explore/_app/immutable/chunks/stores-6b80303c.js",
+  "/explore/_app/immutable/chunks/Disclaimer-15eeef6d.js",
   "/explore/_app/immutable/components/error.svelte-bf878a46.js",
   "/explore/_app/immutable/chunks/_layout-9259b610.js",
-  "/explore/_app/immutable/components/pages/day/_date_/_page.svelte-358eda0d.js",
-  "/explore/_app/immutable/start-bf20037a.js",
-  "/explore/_app/immutable/components/pages/schedule/_page.svelte-f147e7f9.js",
+  "/explore/_app/immutable/components/pages/_page.svelte-5bbbfaa2.js",
+  "/explore/_app/immutable/chunks/CollectionList-8eff242c.js",
   "/explore/_app/immutable/chunks/TimelineHeatmap-8d56020b.js",
-  "/explore/_app/immutable/chunks/Disclaimer-15eeef6d.js",
+  "/explore/_app/immutable/start-bf20037a.js",
   "/explore/_app/immutable/chunks/2-8b5cd56e.js",
   "/explore/_app/immutable/chunks/3-7149fc9a.js",
   "/explore/_app/immutable/chunks/4-431d028c.js",
   "/explore/_app/immutable/chunks/5-6bff8e80.js",
-  "/explore/_app/immutable/chunks/stores-6b80303c.js",
+  "/explore/_app/immutable/chunks/6-ee6d2496.js",
+  "/explore/_app/immutable/modules/pages/_layout.js-e0c98329.js",
+  "/explore/_app/immutable/chunks/CalendarList-5a7545ee.js",
+  "/explore/_app/immutable/components/pages/day/_date_/_page.svelte-358eda0d.js",
+  "/explore/_app/immutable/chunks/0-05ca907b.js",
   "/explore/_app/immutable/chunks/Footer-a3e4efb3.js",
+  "/explore/_app/immutable/chunks/singletons-c9b53750.js",
+  "/explore/_app/immutable/components/pages/schedule/_page.svelte-f147e7f9.js",
+  "/explore/_app/immutable/chunks/1-507f4667.js",
   "/explore/_app/immutable/components/pages/_type_/_page.svelte-a30e5bba.js",
   "/explore/_app/immutable/chunks/EventTypeBadge-9466e62a.js",
   "/explore/_app/immutable/components/pages/_layout.svelte-a85fda49.js",
   "/explore/_app/immutable/components/pages/_type_/_slug_/_page.svelte-49f5b34f.js",
   "/explore/_app/immutable/chunks/utils-157bfc7e.js"
 ], m = [
-  "/explore/bg-prg.png",
   "/explore/favicon.png",
-  "/explore/img/pbw23-banner-square.png",
-  "/explore/img/pbw23-twitter-card.png",
-  "/explore/img/press-center-banner.webp",
+  "/explore/img/.keep",
   "/explore/manifest.json",
   "/explore/preview.jpg"
-], u = "1715893568597", d = "24", h = "Regen Village 2024", b = "https://regenvillage.github.io/data/24", g = "https://regenvillage.github.io/explore", f = "https://github.com/regenvillage/data/tree/main/data/24", _ = [], x = [
+], u = "1715894067556", d = "24", h = "Regen Village 2024", b = "https://regenvillage.github.io/data/24", f = "https://regenvillage.github.io/explore", g = "https://github.com/regenvillage/data/tree/main/data/24", _ = [], x = [
   {
     id: "placeholder",
     hash: "532b295b1631485e9ad90f19b7284118264380d33e6aff2d463d3d92b72ce45e",
@@ -96,12 +93,12 @@ const c = [
       }
     ]
   }
-], v = [], k = [], j = [], y = [], T = "2024-05-16T20:32:34.788Z", w = "v24.2.4", i = {
+], v = [], k = [], j = [], y = [], T = "2024-05-16T20:32:34.788Z", E = "v24.2.4", i = {
   id: d,
   name: h,
   dataUrl: b,
-  exploreUrl: g,
-  dataGithubUrl: f,
+  exploreUrl: f,
+  dataGithubUrl: g,
   places: _,
   events: x,
   "media-partners": [],
@@ -111,8 +108,8 @@ const c = [
   chains: y,
   "other-events": [],
   __time: T,
-  __tag: w
-}, o = {
+  __tag: E
+}, p = {
   tz: "Europe/Brussels",
   collections: {
     places: {
@@ -162,21 +159,21 @@ const c = [
   },
   date: "2024-05-18",
   days: 10
-}, n = `cache-${u}`, l = [];
-for (const e in o.collections)
-  o.collections[e].virtual || l.push(...i[e].map((t) => t.logo || t.photoUrl || t.photo).filter((t) => t));
+}, n = `cache-${u}`, o = [];
+for (const e in p.collections)
+  p.collections[e].virtual || o.push(...i[e].map((t) => t.logo || t.photoUrl || t.photo).filter((t) => t));
 for (const e of i.events)
-  e.speakers && l.push(...e.speakers.map((t) => t.photoUrl).filter((t) => t));
-const r = [
-  ...c,
+  e.speakers && o.push(...e.speakers.map((t) => t.photoUrl).filter((t) => t));
+const c = [
+  ...r,
   // the app itself
   ...m,
   // everything in `static`
-  ...l
+  ...o
 ];
 self.addEventListener("install", (e) => {
   async function t() {
-    await (await caches.open(n)).addAll(r);
+    await (await caches.open(n)).addAll(c);
   }
   e.waitUntil(t());
 });
@@ -192,11 +189,11 @@ self.addEventListener("fetch", (e) => {
     return;
   async function t() {
     const a = new URL(e.request.url), s = await caches.open(n);
-    if (r.includes(a.pathname))
+    if (c.includes(a.pathname))
       return s.match(e.request);
     try {
-      const p = await fetch(e.request);
-      return p.status === 200 && s.put(e.request, p.clone()), p;
+      const l = await fetch(e.request);
+      return l.status === 200 && s.put(e.request, l.clone()), l;
     } catch {
       return s.match(e.request);
     }
