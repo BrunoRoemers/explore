943d85e3c4aaa3898a386bd5fc404da164e95080
BrunoRoemers <BrunoRoemers@users.noreply.github.com>
2024-05-23 21:49:24 +0000

    deploy: d045e384a8a611d80d4040c0cf0343af883198cd

--- _app/immutable/modules/pages/_layout.js-e0c98329.js
+++
@@ -1,1 +0,0 @@
-import{l}from"../../chunks/_layout-9259b610.js";export{l as load};
--- service-worker.js
+++ service-worker.js
@@ -1,55 +1,73 @@
-const r = [
+const c = [
   "/explore/_app/immutable/assets/_layout-faf2005a.css",
-  "/explore/_app/immutable/chunks/Disclaimer-15eeef6d.js",
-  "/explore/_app/immutable/chunks/stores-6b80303c.js",
-  "/explore/_app/immutable/chunks/_layout-9259b610.js",
-  "/explore/_app/immutable/components/error.svelte-bf878a46.js",
-  "/explore/_app/immutable/components/pages/_page.svelte-5bbbfaa2.js",
-  "/explore/_app/immutable/chunks/TimelineHeatmap-8d56020b.js",
   "/explore/_app/immutable/chunks/CollectionList-8eff242c.js",
-  "/explore/_app/immutable/start-bf20037a.js",
-  "/explore/_app/immutable/chunks/3-7149fc9a.js",
-  "/explore/_app/immutable/chunks/0-05ca907b.js",
-  "/explore/_app/immutable/chunks/1-507f4667.js",
-  "/explore/_app/immutable/chunks/2-8b5cd56e.js",
-  "/explore/_app/immutable/chunks/5-6bff8e80.js",
-  "/explore/_app/immutable/chunks/6-ee6d2496.js",
-  "/explore/_app/immutable/modules/pages/_layout.js-e0c98329.js",
-  "/explore/_app/immutable/chunks/EventTypeBadge-9466e62a.js",
+  "/explore/_app/immutable/chunks/Disclaimer-15eeef6d.js",
+  "/explore/_app/immutable/chunks/TimelineHeatmap-8d56020b.js",
   "/explore/_app/immutable/chunks/singletons-c9b53750.js",
-  "/explore/_app/immutable/components/pages/_layout.svelte-a85fda49.js",
+  "/explore/_app/immutable/components/error.svelte-bf878a46.js",
+  "/explore/_app/immutable/chunks/_layout-3bca59e4.js",
+  "/explore/_app/immutable/components/pages/_page.svelte-5bbbfaa2.js",
+  "/explore/_app/immutable/chunks/5-6bff8e80.js",
+  "/explore/_app/immutable/chunks/stores-6b80303c.js",
+  "/explore/_app/immutable/chunks/1-507f4667.js",
+  "/explore/_app/immutable/chunks/3-7149fc9a.js",
   "/explore/_app/immutable/chunks/4-431d028c.js",
   "/explore/_app/immutable/components/pages/schedule/_page.svelte-f147e7f9.js",
-  "/explore/_app/immutable/chunks/CalendarList-5a7545ee.js",
+  "/explore/_app/immutable/chunks/EventTypeBadge-9466e62a.js",
+  "/explore/_app/immutable/chunks/2-8b5cd56e.js",
+  "/explore/_app/immutable/modules/pages/_layout.js-7f49a76d.js",
   "/explore/_app/immutable/chunks/Footer-a3e4efb3.js",
+  "/explore/_app/immutable/chunks/0-9e0b059e.js",
+  "/explore/_app/immutable/chunks/6-ee6d2496.js",
   "/explore/_app/immutable/components/pages/_type_/_page.svelte-a30e5bba.js",
+  "/explore/_app/immutable/start-226be766.js",
   "/explore/_app/immutable/components/pages/day/_date_/_page.svelte-358eda0d.js",
-  "/explore/_app/immutable/chunks/utils-157bfc7e.js",
-  "/explore/_app/immutable/components/pages/_type_/_slug_/_page.svelte-49f5b34f.js"
+  "/explore/_app/immutable/components/pages/_type_/_slug_/_page.svelte-49f5b34f.js",
+  "/explore/_app/immutable/chunks/CalendarList-5a7545ee.js",
+  "/explore/_app/immutable/components/pages/_layout.svelte-a85fda49.js",
+  "/explore/_app/immutable/chunks/utils-157bfc7e.js"
 ], m = [
   "/explore/favicon.png",
   "/explore/img/.keep",
   "/explore/manifest.json",
   "/explore/preview.jpg"
-], u = "1716497312256", d = "24", h = "Regen Village 2024", b = "https://regenvillage.github.io/data/24", f = "https://regenvillage.github.io/explore", g = "https://github.com/regenvillage/data/tree/main/data/24", _ = [], x = [
+], u = "1716500927533", d = "24", h = "Regen Village 2024", b = "https://regenvillage.github.io/data/24", g = "https://regenvillage.github.io/explore", f = "https://github.com/regenvillage/data/tree/main/data/24", _ = [
   {
-    id: "placeholder",
-    hash: "532b295b1631485e9ad90f19b7284118264380d33e6aff2d463d3d92b72ce45e",
-    name: "Example Event 2024",
-    shortname: "Example",
-    types: [
-      "meetup"
+    id: "index",
+    hash: "022b516283e13408676d60ec393eb34b77e5cf680c1f4fb0d4533b80bd0b35fb",
+    name: "Commons Hub",
+    eventTypes: [
+      "gathering"
     ],
-    status: "planning",
-    org: "Example place",
-    dri: "john007",
-    poc: "John Appleseed",
-    date: "2024-05-23",
-    days: 3,
+    capacity: 350,
+    address: "Rue de la Madeleine 51, 1000 Brussels, Belgium",
+    mapUrl: "https://maps.app.goo.gl/x1K2C7pVCW6yVwde7",
+    description: `The Commons Hub Brussels is a shared place to meet, co-create, work and organise events. The space is  managed as a common itself, with shared responsibilities and ownership. All members are crew in making this place thrive.
+`,
+    photo: "https://regenvillage.github.io/data/24/assets/places/index/logo.webp",
+    links: {
+      web: "https://commonshub.brussels/"
+    }
+  }
+], v = [
+  {
+    id: "regen-village",
+    hash: "95af4e21ee513fbc7559747c658301782376454113d260a6eb2b9c1558c1e27f",
+    name: "Regen Village",
+    shortname: "Regen Village",
+    types: [
+      "gathering"
+    ],
+    status: "published",
+    org: "Regens Unite",
+    dri: "Leen Schelfhout",
+    poc: "Leen Schelfhout",
+    date: "2024-07-08",
+    days: 5,
     times: "09:00-20:00",
-    venueName: "Example venue",
-    venueUrl: "https://nominatim.openstreetmap.org/ui/search.html?q=becentral",
-    venueAddress: "Cantersteen 12, 1000 Bruxelles",
+    venueName: "Commons Hub",
+    venueUrl: "https://maps.app.goo.gl/x1K2C7pVCW6yVwde7",
+    venueAddress: "Rue de la Madeleine 51, 1000 Brussels, Belgium",
     languages: [
       "english"
     ],
@@ -58,58 +76,70 @@ const r = [
     tags: [
       "solarpunk"
     ],
-    logo: "https://regenvillage.github.io/data/24/assets/events/placeholder/logo.webp",
+    logo: "https://regenvillage.github.io/data/24/assets/events/regen-village/logo.webp",
     description: `Lorem ipsum ...
 `,
     registration: {
       type: "tickets",
       price: "100 EUR",
-      link: "https://example.org/tickets",
+      link: "https://www.regensunite.earth/event/regenvillage",
       status: "available"
     },
     links: {
-      web: "https://example.org/",
-      twitter: "https://twitter.com/ExampleConf",
-      telegram: "https://t.me/example"
+      web: "https://www.regensunite.earth/event/regenvillage",
+      twitter: "https://twitter.com/regensunite",
+      telegram: "https://t.me/+VM5mWk3S2mthYzM5"
     },
     segments: [
       {
-        date: "2024-05-23",
+        date: "2024-07-08",
         times: "09:00-20:00",
-        startTime: "2024-05-23T07:00:00.000Z",
-        endTime: "2024-05-23T18:00:00.000Z"
+        startTime: "2024-07-08T07:00:00.000Z",
+        endTime: "2024-07-08T18:00:00.000Z"
       },
       {
-        date: "2024-05-24",
+        date: "2024-07-09",
         times: "09:00-20:00",
-        startTime: "2024-05-24T07:00:00.000Z",
-        endTime: "2024-05-24T18:00:00.000Z"
+        startTime: "2024-07-09T07:00:00.000Z",
+        endTime: "2024-07-09T18:00:00.000Z"
       },
       {
-        date: "2024-05-25",
+        date: "2024-07-10",
         times: "09:00-20:00",
-        startTime: "2024-05-25T07:00:00.000Z",
-        endTime: "2024-05-25T18:00:00.000Z"
+        startTime: "2024-07-10T07:00:00.000Z",
+        endTime: "2024-07-10T18:00:00.000Z"
+      },
+      {
+        date: "2024-07-11",
+        times: "09:00-20:00",
+        startTime: "2024-07-11T07:00:00.000Z",
+        endTime: "2024-07-11T18:00:00.000Z"
+      },
+      {
+        date: "2024-07-12",
+        times: "09:00-20:00",
+        startTime: "2024-07-12T07:00:00.000Z",
+        endTime: "2024-07-12T18:00:00.000Z"
       }
     ]
   }
-], v = [], k = [], j = [], y = [], T = "2024-05-16T20:32:34.788Z", E = "v24.2.4", i = {
+], x = [], k = [], T = [], j = [], w = "2024-05-23T21:37:32.118Z", y = "v24.2.6", p = {
   id: d,
   name: h,
   dataUrl: b,
-  exploreUrl: f,
-  dataGithubUrl: g,
+  exploreUrl: g,
+  dataGithubUrl: f,
   places: _,
-  events: x,
+  events: v,
   "media-partners": [],
-  contributors: v,
+  contributors: x,
   benefits: k,
-  unions: j,
-  chains: y,
+  unions: T,
+  chains: j,
   "other-events": [],
-  __time: T,
-  __tag: E
-}, p = {
+  __time: w,
+  __tag: y
+}, o = {
   tz: "Europe/Brussels",
   collections: {
     places: {
@@ -159,28 +189,28 @@ const r = [
   },
   date: "2024-05-18",
   days: 10
-}, n = `cache-${u}`, o = [];
-for (const e in p.collections)
-  p.collections[e].virtual || o.push(...i[e].map((t) => t.logo || t.photoUrl || t.photo).filter((t) => t));
-for (const e of i.events)
-  e.speakers && o.push(...e.speakers.map((t) => t.photoUrl).filter((t) => t));
-const c = [
-  ...r,
+}, i = `cache-${u}`, l = [];
+for (const e in o.collections)
+  o.collections[e].virtual || l.push(...p[e].map((t) => t.logo || t.photoUrl || t.photo).filter((t) => t));
+for (const e of p.events)
+  e.speakers && l.push(...e.speakers.map((t) => t.photoUrl).filter((t) => t));
+const r = [
+  ...c,
   // the app itself
   ...m,
   // everything in `static`
-  ...o
+  ...l
 ];
 self.addEventListener("install", (e) => {
   async function t() {
-    await (await caches.open(n)).addAll(c);
+    await (await caches.open(i)).addAll(r);
   }
   e.waitUntil(t());
 });
 self.addEventListener("activate", (e) => {
   async function t() {
-    for (const a of await caches.keys())
-      a !== n && await caches.delete(a);
+    for (const s of await caches.keys())
+      s !== i && await caches.delete(s);
   }
   e.waitUntil(t());
 });
@@ -188,14 +218,14 @@ self.addEventListener("fetch", (e) => {
   if (e.request.method !== "GET")
     return;
   async function t() {
-    const a = new URL(e.request.url), s = await caches.open(n);
-    if (c.includes(a.pathname))
-      return s.match(e.request);
+    const s = new URL(e.request.url), a = await caches.open(i);
+    if (r.includes(s.pathname))
+      return a.match(e.request);
     try {
-      const l = await fetch(e.request);
-      return l.status === 200 && s.put(e.request, l.clone()), l;
+      const n = await fetch(e.request);
+      return n.status === 200 && a.put(e.request, n.clone()), n;
     } catch {
-      return s.match(e.request);
+      return a.match(e.request);
     }
   }
   e.respondWith(t());
